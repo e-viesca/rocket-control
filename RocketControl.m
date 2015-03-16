1ef5e2e855e19038a1c8a8f22e28fabd554874fc
Eugenio Viesca <eviesca.96@gmail.com>
2015-03-16 16:28:54 +0100

    RK & functions
    Added Runge-Kutta function and corresponding functions. Fixed base for coordinate change. (Still something not working)
    
    Signed-off-by: Eugenio Viesca <eviesca.96@gmail.com>

--- RocketControl.m
+++ RocketControl.m
@@ -27,53 +27,57 @@ clc
 
 % Gravity
 G = 6.667e-11;
-R = 6371000;
+R = 6371009;
 M = 5.9736e24;
 
 % air friction
 k = 0.001; % to be played with
 
 % angular rotation speed of the earth
-w = [0; 0; 7.27e-5];
+w = [0; 0; 0]; %7.27e-5];
 
 %========
 % ROCKET
 %========
 
-% initial conditions
+% Initial conditions
 % Almeria: N36º 50' 17'' W2º 27' 35''
 Lat = (90-36.84) * (pi/180); 
 Lon = (2.46) * (pi/180);
-r0 = [R * sin(Lat) * cos(Lon), R * sin(Lat) * sin(Lon), R * cos(Lat)];
+%Lat = (0) * (pi/180); 
+%Lon = (0) * (pi/180);
+r0 = [R * sin(Lat) * cos(Lon); R * sin(Lat) * sin(Lon); R * cos(Lat)];
 
-AlphaV = (90-45) * (pi/180);
-AlphaH = (-45) * (pi/180);
+AlphaV = (90-  45  ) * (pi/180);
+AlphaH = (     45  ) * (pi/180);
 
-e3 = [sin(AlphaV) * cos(AlphaH)];
-e2 = [sin(AlphaV) * sin(AlphaH)];
-e1 = [cos(AlphaV)];
+e1 = r0 ./ norm(r0);
+e2 = R * [ cos(Lat) * cos(Lon); cos(Lat) * sin(Lon); -sin(Lat)];
+e2 = e2./norm(e2);
+e3 = R * [-sin(Lat) * sin(Lon); sin(Lat) * cos(Lon);     0    ];
+e3 = e3./norm(e3);
 
-v0 = 5000 * [e1 , e2, e3];
-% v0 = (...);
+v0 = 1 * [sin(AlphaV) * cos(AlphaH); sin(AlphaV) * sin(AlphaH); cos(AlphaV)];
+%%%%%v0 = [e1, e2, e3] * v0;
 
 %=============
 % INTEGRATION
 %=============
 t0 = 0;
 tN = 2*norm(v0)/(G*M/R^2);
-N = 1000;
+N = 10000;
 
 %===============
 % ACCELERATIONS
 %===============
 % gravity
-ag = @(r,v,t) ( -((G*M)./(r.^2)) );
+ag = @(r,v,t) ( -((G*M)./norm(r)^3 .* r) );
 % air friction
-af = @(r,v,t) ( -k * v);
+af = @(r,v,t) ( -k .* v);
 % centripetal force
 ac = @(r,v,t) ( cross(w, cross(w, r)) );
 % Coriolis force
-a =  @(r,v,t) ( cross(2.*w, v) );
+aC =  @(r,v,t) ( cross(2.*w, v) );
 
 %{
 % propulsion (NO NO for now)
@@ -84,22 +88,23 @@ dmdt = @(t) ();
 %========================
 % DIFFERENTIAL EQUATIONS
 %========================
-g = @(r,v,t) ();
-h = @(r,v,t) ();
+g = @(r,v,t) (v);
+h = @(r,v,t) (ag(r,v,t) + af(r,v,t) + ac(r,v,t) + aC(r,v,t));
 
 [r,v,t] = rocketRK2(g,h,r0,v0,t0,tN,N);
-
 %===============
 % VISUALISATION
 %===============
+quiver3(0,0,0,v0(1),v0(2),v0(3))
+%%
 figure(1)
 hold on
 
 % plot starting point
-plot3(r0(1),r0(2),r0(3),'rx','MarkerSize',20,'LineWidth',4)
+plot3(r0(1),r0(2),r0(3),'rx','MarkerSize',20,'LineWidth',3)
 
 % plot trajectory
-%plot3(r(1,:),r(2,:),r(3,:),'.','LineWidth',20)
+plot3(r(1,:),r(2,:),r(3,:),'.','LineWidth',20)
 
 % plot Earth
 [x,y,z] = sphere(30);
---
+++ rocketRK2.m
@@ -0,0 +1,33 @@
+function [ r,v,t ] = rocketRK2( g, h, r0, v0, t0, tN, N )
+%rocketRK2 Summary of this function goes here
+%   Detailed explanation goes here
+
+% initialisation block
+%----------------------
+% step size
+dt   = (tN-t0)/(N-1);
+
+% arrays
+t    = linspace(t0,tN,N);
+r    = [zeros(1,N);zeros(1,N);zeros(1,N)];
+v    = [zeros(1,N);zeros(1,N);zeros(1,N)];
+%t(1) = t0; this is automatically initialized by linspace()
+r(:,1) = r0;
+v(:,1) = v0;
+
+
+% integration block
+%-------------------
+for i=1:N-1
+    
+    % mid-point values
+    tmid = t(i) + dt/2;
+    xmid = r(:,i) + g(r(:,i),v(:,i),t(i)) .* dt/2;
+    vmid = v(:,i) + h(r(:,i),v(:,i),t(i)) .* dt/2;
+    
+    % actual step
+    r(:,i+1) = r(:,i) + g(xmid,vmid,tmid).*dt;
+    v(:,i+1) = v(:,i) + h(xmid,vmid,tmid).*dt;
+        
+end
+
